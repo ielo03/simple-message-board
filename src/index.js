f0d91bb12dc9ea13aec8c7bdcc2d6413f51af055
ielo03 <89800883+ielo03@users.noreply.github.com>
2024-10-09 12:35:45 -0700

    Switched to port 80

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
